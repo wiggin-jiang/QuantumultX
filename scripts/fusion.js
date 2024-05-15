37b5278ab037ac7dde2a26c2bc7bf62529587275
wiggin-jiang <137773913+wiggin-jiang@users.noreply.github.com>
2024-05-15 09:32:21 +0800

    Update fusion.js

--- scripts/fusion.js
+++ scripts/fusion.js
@@ -1,12 +1,12 @@
 const name = "获取丰声token";
 const $ = new Env(name);
 
-$.msg($.name,'' , 'test');
-async function getToken() {
+$.msg($.name, '', 'test');
+function getToken() {
   try {
     const headers = ObjectKeys2LowerCase($request.headers);
     
-    let currentToken = $request.headers['token'];
+    let currentToken = headers['token'];
     $.msg(name, '', currentToken);
   } catch (err) {
     $.logger.error(`获取token异常，${err}`);
